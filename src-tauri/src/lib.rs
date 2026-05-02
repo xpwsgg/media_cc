d8c4f57352c7d51c663aa29e2662f91be0fb6b57
xiao <xpwsgg@163.com>
2026-05-02 11:44:53 +0800

    feat: add single-instance enforcement and Windows portable build

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -460,6 +460,12 @@ fn open_terminal_with_command(command: String) -> Result<(), String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.show();
+                let _ = window.set_focus();
+            }
+        }))
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
